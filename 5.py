d516356ab8496edbddf192eac7d86acbb00d8617
Xenoker <xenoker@users.noreply.github.com>
2020-12-05 23:35:23 -0500

    Updated 5 with realization

--- 5.py
+++ 5.py
@@ -2,9 +2,7 @@ from puzzleinput import getlines
 INPUT = getlines(5)
 CHAR = dict(zip('FBLR','0101'))
 def seat(code):
-    row = int(''.join(CHAR[x] for x in code[:7]),2)
-    col = int(''.join(CHAR[x] for x in code[-3:]),2)
-    return row*8+col
+    return int(''.join(CHAR[x] for x in code),2)
 seats = sorted(seat(x) for x in INPUT)
 def part1(): return max(seats)
 def part2(): return [x+1 for x,y in zip(seats[:-1],seats[1:]) if y-x==2][0]
