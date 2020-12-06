38cc53b28ebd50c1899f3addffe5cbe1ed79c411
Xenoker <xenoker@users.noreply.github.com>
2020-12-05 23:37:42 -0500

    removed a line

--- 5.py
+++ 5.py
@@ -1,8 +1,7 @@
 from puzzleinput import getlines
 INPUT = getlines(5)
 CHAR = dict(zip('FBLR','0101'))
-def seat(code):
-    return int(''.join(CHAR[x] for x in code),2)
+def seat(code): return int(''.join(CHAR[x] for x in code),2)
 seats = sorted(seat(x) for x in INPUT)
 def part1(): return max(seats)
 def part2(): return [x+1 for x,y in zip(seats[:-1],seats[1:]) if y-x==2][0]
