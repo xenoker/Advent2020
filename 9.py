0d82adcab075510e93dc297f754757e7a2a8bef5
Xenoker <xenoker@users.noreply.github.com>
2020-12-09 01:04:14 -0500

    Day 9

---
+++ 9.py
@@ -0,0 +1,17 @@
+from puzzleinput import getints
+INPUT = getints(9)
+def part1(nums,P):
+    for i,n in enumerate(nums[P:],P):
+        pre = INPUT[i-P:i]
+        if not [x for x in pre if n-x in pre]: return n,i
+def part2(nums,find,at):
+    for i in range(at-2,-1,-1): #loop by initial position first
+        for n in range(2,at-i): #so number of ints can abort
+            L = nums[i:i+n]
+            S = sum(L)
+            if S > find: break #as soon as its too large
+            if S == find: return min(L)+max(L)
+P1,at = part1(INPUT,25)
+P2 = part2(INPUT,P1,at)
+assert P1 == 1398413738
+assert P2 == 169521051
